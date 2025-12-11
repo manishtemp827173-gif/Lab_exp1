1fd5dde8a972d797c16546b24cc3eb6f4e0e86f4
manishtemp827173-gif <manishtemp827173@gmail.com>
2025-12-11 13:55:37 +0530

    Add files via upload

---
+++ fsw_pr/Client .java
@@ -0,0 +1,18 @@
+import java.rmi.*;
+
+public class Client {
+    public static void main(String a[]) throws Exception {
+        AddI obj = (AddI) Naming.lookup("ADD");
+        int n = obj.add(5, 4);
+        System.out.println("addition is:" + n);
+    }
+
+}
+
+
+
+
+
+
+
+
